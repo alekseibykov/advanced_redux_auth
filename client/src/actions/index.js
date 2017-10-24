14cbe8f1eb689180330daff1f05387d91a32ee78
Aleksei <gogohandz@gmail.com>
2017-10-24 19:58:56 +0300

    Add auth state reducer and saved token to local storage

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import history from '../history';
+import { AUTH_USER } from './types';
 
 const ROOT_URL = 'http://localhost:3090';
 
@@ -7,6 +8,10 @@ export function signinUser({ email, password }) {
   return function(dispatch) {
     axios.post(`${ROOT_URL}/signin`, { email, password })
       .then(response => {
+        dispatch({ type: AUTH_USER });
+
+        localStorage.setItem('token', response.data.token);
+
         history.push('/feature');
       })
       .catch(() => {
@@ -14,4 +19,4 @@ export function signinUser({ email, password }) {
       })
 
   };
-};
+}
